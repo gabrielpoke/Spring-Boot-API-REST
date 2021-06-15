0186a75342a92c07717ca42513038f9903f61c0a
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2021-06-14 22:13:35 -0300

    Criando consultas com filtro

--- src/main/java/br/com/alura/forum/Repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/Repository/TopicoRepository.java
@@ -3,5 +3,10 @@ package br.com.alura.forum.Repository;
 import br.com.alura.forum.Models.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    List<Topico> findByCursoNome(String nomeCurso);
+
 }
