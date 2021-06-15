fe65bc703248976d657afdbb16e7512adf5c3f3c
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2021-06-15 13:18:39 -0300

    Mensagem personalizada de erros de validação Handler

---
+++ src/main/java/br/com/alura/forum/Config/Validation/ErrodeFormularioDto.java
@@ -0,0 +1,21 @@
+package br.com.alura.forum.Config.Validation;
+
+
+public class ErrodeFormularioDto {
+
+    private String campo;
+    private String erro;
+
+    public ErrodeFormularioDto(String campo, String erro) {
+        this.campo = campo;
+        this.erro = erro;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public String getErro() {
+        return erro;
+    }
+}
